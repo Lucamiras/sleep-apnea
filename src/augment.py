63da97646a1dfd0dafa470e0a9962dfbe6bd3f31
lucamiras <lucamiras@googlemail.com>
2024-07-05 15:15:50 +0200

    Implemented SpecAugmentation

---
+++ src/augment.py
@@ -0,0 +1,57 @@
+import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+import random
+import os
+
+class SpecAugmentation:
+    def __init__(self, train_dir: str, augmentations: list =['time','frequency'], subset: float = 0.4, sample_rate: int = 48_000) -> None:
+        self.train_dir = train_dir
+        self.augmentations = augmentations
+        self.subset = subset
+        self.sample_rate = sample_rate
+        self.files_to_augment = self._get_subset_of_files()
+        print(f"SpecAugment initialized with {augmentations} and subset size of {subset}.")
+
+    def _get_subset_of_files(self) -> list:
+        train_files = os.listdir(self.train_dir)
+        train_files = [file.replace('.png', '.wav') for file in train_files]
+        train_files = [os.path.join(self.train_dir, file) for file in train_files]
+        subset_index = int(len(train_files) * self.subset)
+        random.shuffle(train_files)
+        return train_files[:subset_index]
+
+    def _time_mask(self, spec, T=20):
+        t = spec.shape[1]
+        t0 = random.randint(0, t - T)
+        spec[:, t0:t0 + T] = 0
+        return spec
+
+    def _freq_mask(self, spec, F=15):
+        f = spec.shape[0]
+        f0 = random.randint(0, f - F)
+        spec[f0:f0 + F, :] = 0
+        return spec
+
+    def _aug_pipeline(self, spec, T=20, F=15):
+        assert len(self.augmentations) > 0, "No augmentations specified"
+        
+        if 'time' in self.augmentations:
+            spec = self._time_mask(spec, T)    
+        if 'frequency' in self.augmentations:
+            spec = self._freq_mask(spec, F)
+        
+        return spec
+    
+    def run(self):
+        
+        output_dir = os.path.join('data', 'processed', 'spectrogram', 'augmented')
+        os.makedirs(output_dir, exist_ok = True)
+        
+        for file in self.files_to_augment:
+            spec = self._aug_pipeline(file)
+            plt.figure(figsize=(4, 4))
+            librosa.display.specshow(spec, sr=self.sample_rate, x_axis='time', y_axis='mel')
+            plt.axis('off')
+            plt.savefig(output_dir, bbox_inches='tight', pad_inches=0)
+            plt.close()

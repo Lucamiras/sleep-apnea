4236fe6a3284d491df9c01490b81efbdcd4ea1f4
lucamiras <lucamiras@googlemail.com>
2024-10-05 18:20:17 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ from src.utils.globals import (
     CLASSES
 )
 
-config = Config(project_dir='data', classes=CLASSES, download_files=False, extract_signals=False)
+# Initialize pipeline elements
+config = Config(classes=CLASSES, download_files=False, extract_signals=False)
 downloader = Downloader(config)
 extractor = Extractor(config)
 processor = Processor(config)
 serializer = Serializer(config)
 
+# Generate download URLS and update config class
 edf_urls, rml_urls, unique_ids = downloader.get_download_urls()
-
 config.edf_urls = edf_urls
 config.rml_urls = rml_urls
 
+# Initialize full pipeline
 pre = DataPreprocessor(
     downloader,
     extractor,
     processor,
     serializer,
     config)
-
-print(pre.config.edf_urls)

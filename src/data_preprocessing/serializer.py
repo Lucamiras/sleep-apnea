67f1c82497c3f03ffe783855e5eda6da1f6aeb65
lucamiras <lucamiras@googlemail.com>
2025-01-19 16:26:07 +0100

    Improved serializer code for readability

--- src/data_preprocessing/serializer.py
+++ src/data_preprocessing/serializer.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import shutil
-from src.dataclasses.spectrogram import SpectrogramDataset
+from src.dataclasses.spectrogram import Spectrogram, SpectrogramDataset
 from src.data_preprocessing.config import Config
 
 
@@ -18,4 +18,5 @@ class Serializer:
         retired_file_path = os.path.join(self.config.retired_path, self.config.dataset_file_name)
         if self.config.dataset_file_name in os.listdir(self.config.signals_path):
             shutil.move(src=dataset_file_path, dst=retired_file_path)
-        pickle.dump(self.dataset, open(dataset_file_path, "wb"))
+        with open(dataset_file_path, "wb") as file:
+            pickle.dump(self.dataset, file)
